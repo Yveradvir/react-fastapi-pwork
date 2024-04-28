da33c70a71ea9386b79425a032da838fa2e768ef
Yveradvir <yveradvirr@gmail.com>
2024-04-28 17:12:36 +0300

    Templates for posts was added

---
+++ frontend/src/app/post/add.post.tsx
@@ -0,0 +1,11 @@
+import Layout from "@modules/components/layout"
+
+const AddPost: React.FC = () => {
+    return (
+        <Layout>
+            <h1>{import.meta.env.VITE_A}</h1>
+        </Layout>
+    );
+}
+
+export default AddPost;
---
+++ frontend/src/app/post/single.post.tsx
@@ -0,0 +1,12 @@
+import Layout from "@modules/components/layout"
+
+const SignlePost: React.FC = () => {
+    
+    return (
+        <Layout>
+            <h1>{import.meta.env.VITE_A}</h1>
+        </Layout>
+    );
+}
+
+export default SignlePost;
