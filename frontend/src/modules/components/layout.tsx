643934acfe8b3dc442723077aedbd5c57576f89b
Yveradvir <yveradvirr@gmail.com>
2024-04-30 17:29:31 +0300

    The post base was created

--- frontend/src/modules/components/layout.tsx
+++ frontend/src/modules/components/layout.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Container } from "react-bootstrap";
 import Header from "./header";
-import DownProps from './downprops';
 
 interface Props {
     children: React.ReactNode
-    downprops?: boolean;
 }
 
-const Layout: React.FC<Props> = ({ children, downprops=true }) => {
+const Layout: React.FC<Props> = ({ children }) => {
     return (
         <div>
             <Header />
             <Container>
                 {children}
             </Container>
-            {downprops && <DownProps/>}
         </div>
     );
 } 
